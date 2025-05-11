fe39e704d8bfee9150ea7d1c4ea694f18bdfdc92
Gautham V <gauthamvr2005@gmail.com>
2025-05-11 14:33:10 +0530

    responsive fixed

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/makemypass-frontend-t1",
-  server: {
-    allowedHosts: [
-      '2a2a-103-165-167-220.ngrok-free.app' // Add your ngrok domain here
-    ]
-  }
 })
