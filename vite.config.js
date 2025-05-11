cc9a6928346cafdf9433e93aa0817762551ef108
Gautham V <gauthamvr2005@gmail.com>
2025-05-11 14:23:43 +0530

    media query width fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/makemypass-frontend-t1"
+  base: process.env.VITE_BASE_PATH || "/makemypass-frontend-t1",
+  server: {
+    allowedHosts: [
+      '2a2a-103-165-167-220.ngrok-free.app' // Add your ngrok domain here
+    ]
+  }
 })
